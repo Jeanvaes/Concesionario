4b4da92eab35b63512b30a775c40e5dfa0873b67
jeanvaes <jeanvaes@unisabana.edu.co>
2022-05-27 16:09:13 -0500

    Agregar archivos de proyecto.

---
+++ Concesionario/Data/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concesionario.Data
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int numero { get; set; }
+        public string nombre { get; set; }
+        public string sueldo { get; set; }
+        public int numeroCarros { get; set; }
+        public string marcaPreferida { get; set; }
+    }
+}
---
+++ Concesionario/Data/Garaje.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concesionario.Data
+{
+    public class Garaje
+    {
+       
+            [Key]
+            public int serial { get; set; }
+            public string marca { get; set; }
+            public int precio { get; set; }
+            public string tipo { get; set; }
+            public int power { get; set; }
+        
+    }
+}
